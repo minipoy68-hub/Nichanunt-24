1b389b1df63daba97ce95a01a86d0aa4def249f4
minipoy68-hub <minipoy68@gmail.com>
2026-09-17 10:10:06 +0700

    Update Examination_5.py

--- Examination_5.py
+++ Examination_5.py
@@ -7,4 +7,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-bmi = float(input())
+bmi = float(input("กรอกค่าbmiของคุณ: "))
+if bmi < 18.5:
+   print("Underweight")
+elif bmi <=22.9:
+  print("Normol")
+else:
+   print("Overweight")
